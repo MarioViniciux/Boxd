d87bddfbf6b278361b0d62d6268b2fdd182091d7
Mario Vinicius <marioviniciuxcs@gmail.com>
2025-01-07 17:44:52 -0300

    :construction::build: inicio na página lists

---
+++ src/books.ts
@@ -0,0 +1,40 @@
+const books = [{
+    id: 1,
+    title: "O Senhor dos Anéis",
+    coverUrl: "https://m.media-amazon.com/images/I/A1xRL6qXHOL._AC_UL320_.jpg",
+    link: "https://www.amazon.com.br/s?k=O+Senhor+dos+Anéis",
+},{
+    id: 2,
+    title: "O Alquimista",
+    coverUrl: "https://m.media-amazon.com/images/I/915lTnCKqvL._AC_UL320_.jpg",
+    link: "https://www.amazon.com.br/s?k=O+Alquimista",
+},    {
+    id: 3,
+    title: "A Guerra dos Mundos",
+    coverUrl: "https://m.media-amazon.com/images/I/71w172G57mS._AC_UY218_.jpg",
+    link: "https://a.co/d/dlxIR4R"
+  },
+  {
+    id: 4,
+    title: "1984",
+    coverUrl: "https://m.media-amazon.com/images/I/81l3pUGbT8L._AC_UL320_.jpg",
+    link: "https://www.amazon.com.br/s?k=1984"
+  },
+  {
+    id: 5,
+    title: "Duna",
+    coverUrl: "https://m.media-amazon.com/images/I/81zN7udGRUL._AC_UY218_.jpg",
+    link: "https://a.co/d/71F4Qwr"
+  },
+  {
+    id: 6,
+    title: "O Conto da Aia",
+    coverUrl: "https://m.media-amazon.com/images/I/915d8NRLU-L._AC_UY218_.jpg",
+    link: "https://a.co/d/5m4mBaG"
+  },
+  {
+    id: 7,
+    title: "Neuromancer",
+    coverUrl: "https://m.media-amazon.com/images/I/91Bx5ilP+EL._AC_UY218_.jpg",
+    link: "https://a.co/d/iJsCJHq"
+  },]
